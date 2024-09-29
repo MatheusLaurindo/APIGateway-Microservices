aa330bbbcbd4ab484811587b795875ebb6c14ae7
Matheus Figueredo <matheuslaurindo2017@gmail.com>
2024-09-29 19:42:53 -0300

    initial

---
+++ Books-BusinessObjects/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using Books_BusinessObjects.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Books_BusinessObjects.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ BooksService/Infraestructure/BookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Books_BusinessObjects.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksService.Infraestructure
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
