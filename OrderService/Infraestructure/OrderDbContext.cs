c47d8fdeefb06680431bd1cab02495353bcf2f65
Matheus Figueredo <matheuslaurindo2017@gmail.com>
2024-09-30 11:06:03 -0300

    microsservico de orders

---
+++ OrderService/Infraestructure/OrderDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Books_BusinessObjects.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderService.Infraestructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
